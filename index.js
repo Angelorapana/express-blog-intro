6e06de9e9a2a73c841b8139c13839010e0da325d
Angelo Rapanà <angelorapana@icloud.com>
2026-03-25 16:58:36 +0100

    Aggiunta array e modificata rotta bacheca

--- index.js
+++ index.js
@@ -6,13 +6,45 @@ const app = express()
 
 app.use(express.static("public")) //Necessario per gli assets statici (css, js, immagini)
 
+const posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "Dolce soffice perfetto per la colazione",
+    immagine: "/images/ciambellone.jpeg",
+    tags: ["dolce", "colazione"]
+  },
+  {
+    titolo: "Cracker alla barbabietola",
+    contenuto: "Snack croccante e alternativo",
+    immagine: "/images/cracker-barbabietola.jpeg",
+    tags: ["snack", "salato"]
+  },
+  {
+    titolo: "Pane fritto dolce",
+    contenuto: "Ricetta semplice e golosa",
+    immagine: "/images/pane-fritto-dolce.jpeg",
+    tags: ["dolce", "tradizione"]
+  },
+  {
+    titolo: "Pasta alla barbabietola",
+    contenuto: "Primo piatto colorato e saporito",
+    immagine: "/images/pasta-barbabietola.jpeg",
+    tags: ["primo", "vegetariano"]
+  },
+  {
+    titolo: "Pasta paesana",
+    contenuto: "Piatto rustico e ricco di sapore",
+    immagine: "/images/pasta-paesana.jpeg",
+    tags: ["primo", "tradizione"]
+  }
+]
 
 app.get("/", (req, res) => {
     res.send("Home")
 })
 
 app.get("/bacheca", (req, res) => {
-    res.send("Bacheca")
+    res.json(posts)
 })
 
 app.listen(3000, () => {
