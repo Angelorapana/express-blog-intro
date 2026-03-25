5cb10e2650b24fe43025443724efb5fb527f2412
Angelo Rapanà <angelorapana@icloud.com>
2026-03-25 16:51:20 +0100

    Fix .use per visualizzazione asset statici

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("Hello world!");
+
+const express = require("express")
+const app = express()
+
+
+app.use(express.static("public")) //Necessario per gli assets statici (css, js, immagini)
+
+
+app.get("/", (req, res) => {
+    res.send("Home")
+})
+
+app.get("/bacheca", (req, res) => {
+    res.send("Bacheca")
+})
+
+app.listen(3000, () => {
+  console.log("Server attivo su porta 3000")
+})
